7cc7892265a7784d01c347b7ae64b9ab77ebbff8
TusharMorankar <119660791+TusharMorankar@users.noreply.github.com>
2023-01-17 14:59:50 +0530

    Add files via upload

---
+++ Program258.java
@@ -0,0 +1,24 @@
+
+class ArrayX
+{
+    public int Arr[];
+
+    public ArrayX(int iSize)
+    {
+        Arr = new int [iSize];
+    }
+
+    void Accept()
+    {
+        
+    }
+
+}
+class Program258
+{
+
+    public static void main(String ar[])
+    {
+        ArrayX obj = new ArrayX(5);
+    }
+}
---
+++ Program268.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Program268
+{
+    public static void main(String Ar[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("PLEASE ENTER YOUR FULL Name: ");
+        String str = sobj.nextLine();
+
+        System.out.println("Number of characters are:" +str.Lenght());
+    }
+}
